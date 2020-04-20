a7d59be4fb0c4356d7173470e6eac65f14199870
jrharting <jharting@pacificbiosciences.com>
2020-04-20 15:56:13 -0700

    typo

--- setup.py
+++ setup.py
@@ -3,13 +3,19 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["scikit-learn>=0.22", "numpy", "pandas>=1", "pysam>=0.15", "mappy>=2.17","matplotlib>=3", "seaborn>=0.10", "scipy"]
+requirements = ["scikit-learn>=0.22",
+                "numpy",
+                "pandas>=1",
+                "pysam>=0.15",
+                "mappy>=2.17","matplotlib>=3",
+                "seaborn>=0.10",
+                "scipy"]
 
 setup(
     name="pbampliconphasing",
     version="0.0.1",
     author="John Harting",
-    author_email="jharting@pacifivbiosciences.com",
+    author_email="jharting@pacificbiosciences.com",
     description="Tools for clustering PB amplicons",
     long_description=readme,
     long_description_content_type="text/markdown",
